c000eec4589d3b6a4e0cbfa86e27ce27a629fa0b
Uuu9 <87708661+GUuu9@users.noreply.github.com>
2023-02-06 21:02:27 +0900

    Add files via upload

--- study_GUI_python.py
+++ study_GUI_python.py
@@ -69,17 +69,62 @@ def naver_news(event): # naver 뉴스 크롤링
     browser = webdriver.Chrome()
     
     # 검색 URL 바로 이동
-    url = "https://news.naver.com/"
+    url = "https://search.naver.com/search.naver?where=news&sm=tab_jum&query={}"
+    new_url = url.format(input_box2.get()) 
+    browser.get(new_url)
+    
+    csv = open("naver_news.csv", "a")
+    csv.write("번호, 기사 제목, 기사 요약, 링크\n")
+    csv.close()
+    
+    title_list = []
+    content_list = []
+    link_list = []
+    
+    for i in range(5): # 페이지 이동
+        
+        # 제목, 내용, 링크
+        titles = browser.find_elements(By.CLASS_NAME, 'news_tit')
+        contents = browser.find_elements(By.CLASS_NAME, 'news_dsc')
+        links = browser.find_elements(By.CLASS_NAME, 'news_tit')
+        
+        for i in titles:
+            title = i.text
+            title = title.replace(",", "")
+            title_list.append(title)
+
+        for i in contents:
+            content = i.text
+            content = content.replace("," , "")
+            content_list.append(content)
+            
+        for i in links:
+            link = i.get_attribute('href')
+            link_list.append(link)
+
+        browser.find_element(By.CLASS_NAME, "btn_next").click()
+        time.sleep(5)
+        
+    for i in range(len(title_list)):
+            csv = open("naver_news.csv", "a")
+            csv.write(f"{i}, {title_list[i]}, {content_list[i]}, {link_list[i]}\n")
+            csv.close()
+
+def thesis_craw(event): # Riss 논문 크롤링
+    browser = webdriver.Chrome()
+    
+    # 검색 URL 바로 이동
+    url = "http://www.riss.or.kr/search/Search.do?isDetailSearch=N&searchGubun=true&viewYn=OP&query=지방&queryText=&iStartCount=0&iGroupView=5&icate=re_a_kor&colName=re_a_kor&exQuery=&exQueryText=&order=%2FDESC&onHanja=false&strSort=RANK&pageScale=10&orderBy=&fsearchMethod=search&isFDetailSearch=N&sflag=1&searchQuery=지방&fsearchSort=&fsearchOrder=&limiterList=&limiterListText=&facetList=&facetListText=&fsearchDB=&resultKeyword=지방&pageNumber=1&p_year1=&p_year2=&dorg_storage=&mat_type=&mat_subtype=&fulltext_kind=&t_gubun=&learning_type=&language_code=&ccl_code=&language=&inside_outside=&fric_yn=&image_yn=&regnm=&gubun=&kdc=&ttsUseYn="
     browser.get(url)
     
     csv = open("naver_news.csv", "a")
     csv.write("번호, 기사 제목, 기사 요약, 링크\n")
     csv.close()
     
-    browser.find_element(By.CLASS_NAME, 'Nicon_search').click()
-    browser.find_element(By.CLASS_NAME, 'u_it').click()
-    browser.find_element(By.CLASS_NAME, 'u_it').send_keys(input_box2.get())
-    browser.find_element(By.CLASS_NAME, 'u_hssbt').click()
+    browser.find_element(By.XPATH, '//*[@id="query"]').click()
+    browser.find_element(By.XPATH, '//*[@id="query"]').send_keys(input_box3.get())
+    browser.find_element(By.CLASS_NAME, 'btnSearch').click()
+    
     
     title_list = []
     content_list = []
@@ -139,11 +184,13 @@ input_box2.grid(column=2, row=3)
 input_box2.bind("<Return>", naver_news)
 
 
+# Riss
+file_text3 = Label(window, text="Riss 논문 크롤링")
+file_text3.grid(column=1, row=4)
 
-# file_text2 = Button(window, text="논문 데이터 크롤링")
-# file_text2.grid(column=1, row=4)
+input_box3 = Entry(window, width=20)
+input_box3.grid(column=2, row=4)
+input_box3.bind("<Return>", thesis_craw)
 
-# file_btn = Button(window, text="파일선택", command=get_file)
-# file_btn.grid(column=3, row=3)
 
 window.mainloop()
